9c12f4b0e332c376ba7f2fa610c6c4e151589014
Gregoire <53735@etu.he2b.be>
2020-03-16 21:58:32 +0100

    Classe Square

---
+++ g53735-Humbug/src/main/java/g53735/humbug/model/Square.java
@@ -0,0 +1,30 @@
+package g53735.humbug.model;
+
+/**
+ * Square on the board. A square has a type grass or star and it’s all. A square
+ * does not know where it is on the board.
+ *
+ * @author g53735
+ */
+public class Square {
+
+    private SquareType type;
+
+    /**
+     * Constructor of Square on board.
+     *
+     * @param type Square is grass or star
+     */
+    public Square(SquareType type) {
+        this.type = type;
+    }
+
+    /**
+     * Simple getter of type.
+     *
+     * @return type of Square
+     */
+    public SquareType getType() {
+        return type;
+    }
+}
