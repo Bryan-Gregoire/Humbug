97d090467a53094aa48471fb18fbe015cefdae39
Gregoire <53735@etu.he2b.be>
2020-03-16 22:37:49 +0100

    Classe position

---
+++ g53735-Humbug/src/main/java/g53735/humbug/model/Position.java
@@ -0,0 +1,81 @@
+package g53735.humbug.model;
+
+/**
+ * Position on the board. A position has a row and column that form a position.
+ *
+ * @author g53735
+ *
+ */
+public class Position {
+
+    private final int row;
+    private final int column;
+
+    /**
+     * Constructor of a position on the board.
+     *
+     * @param row the given row of a position.
+     * @param column the given column of a position.
+     */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * Get the value of a row.
+     *
+     * @return the value of row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Get the value of a column.
+     *
+     * @return the value of column
+     */
+    public int getColumn() {
+        return column;
+    }
+
+    /**
+     * @return a hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.row;
+        hash = 97 * hash + this.column;
+        return hash;
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param obj the reference object with which to compare.
+     * @return true if this object is the same as the obj argument; false
+     * otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Position other = (Position) obj;
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.column != other.column) {
+            return false;
+        }
+        return true;
+    }
+}
--- g53735-Humbug/src/main/java/g53735/humbug/model/SquareType.java
+++ g53735-Humbug/src/main/java/g53735/humbug/model/SquareType.java
@@ -1,9 +1,9 @@
 package g53735.humbug.model;
 
 /**
- * SquareType represents the type of a square on the board.
- * Square are grass or star when there represent arrival.
- * 
+ * SquareType represents the type of a square on the board. Square are grass or
+ * star when there represent arrival.
+ *
  * @author g53735
  */
 public enum SquareType {
