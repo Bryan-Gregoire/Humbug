6543f996d37d896426b01f4426c7881f65304f53
Gregoire <53735@etu.he2b.be>
2020-04-10 18:11:22 +0200

    Ajout de la sauterelle, modification du move(pas complète).

---
+++ g53735-Humbug/src/main/java/g53735/humbug/model/Grasshopper.java
@@ -0,0 +1,55 @@
+package g53735.humbug.model;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author g53735
+ */
+public class Grasshopper extends Animal {
+
+    /**
+     * Constructor of grasshopper.
+     *
+     * @param positionOnBoard the position of grasshopper on the game board.
+     */
+    public Grasshopper(Position positionOnBoard) {
+        super(positionOnBoard);
+    }
+
+    @Override
+    public Position move(Board board, Direction drection, Animal... animals) {
+        Position grasshopper = this.getPositionOnBoard();
+        Position nextPos = grasshopper.next(drection);
+
+        if (!board.isInside(nextPos)) {
+            this.setPositionOnBoard(null);
+            return null;
+        }
+
+        boolean free = true;
+        while (free) {
+            for (Animal animal : animals) {
+                if (animal.getPositionOnBoard().equals(nextPos)) {
+                    free = false;
+                }
+            }
+            if (!free) {
+                grasshopper = nextPos;
+                nextPos = grasshopper.next(drection);
+
+                if (!board.isInside(nextPos)) {
+                    this.setPositionOnBoard(null);
+                    return null;
+                }
+            } 
+        }
+        grasshopper = nextPos;
+        if (board.getSquareType(grasshopper) == SquareType.STAR) {
+            this.setOnStar(true);
+            board.setSquareType(grasshopper, SquareType.GRASS);
+        }
+        return grasshopper;
+    }
+
+}
